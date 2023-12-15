6fc4ee44fad3bbab3cd33e732d3b27f0467eb384
Felix chinonso Emmanuel <Feelchi1star@gmail.com>
2023-12-15 22:17:41 +0100

    Completed ENV

--- web-server/controller/sectorsController.js
+++ web-server/controller/sectorsController.js
@@ -1,6 +1,5 @@
 const catchAsync = require("../utils/catchAsync");
 const sectorModel = require("../models/sectorsModel");
-const sectorModelSUB = require("../models/subSectorsModel");
 
 module.exports.getALLSectors = catchAsync(async (req, res, next) => {
   const sectors = await sectorModel.find({}).populate({
@@ -15,27 +14,4 @@ module.exports.getALLSectors = catchAsync(async (req, res, next) => {
     },
   }); // Populate the 'children' field in the nested documents
   res.status(200).json(sectors);
-});
-
-module.exports.createSectors = catchAsync(async (req, res, next) => {
-  const sectors = await sectorModel.insertMany([
-    { value: "1", label: "Manufacturing" },
-    { value: "2", label: "Service" },
-    { value: "437", label: "Other" },
-  ]);
-  return res.json(sectors);
-});
-module.exports.createSubSector = catchAsync(async (req, res, next) => {
-  const sectors = await sectorModelSUB.insertMany(req.body.items);
-  const sector = await sectorModelSUB.updateOne(
-    {
-      _id: req.body.idx,
-    },
-    {
-      $push: {
-        children: sectors.map((i) => i._id),
-      },
-    }
-  );
-  return res.json(sector);
 });
