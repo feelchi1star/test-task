5370f82873cdad139911dc072f7580b384849d3e
Felix chinonso Emmanuel <Feelchi1star@gmail.com>
2024-01-03 21:35:12 +0100

    Sectors Queries

--- web-server/controller/sectorsController.js
+++ web-server/controller/sectorsController.js
@@ -2,16 +2,16 @@ const catchAsync = require("../utils/catchAsync");
 const sectorModel = require("../models/sectorsModel");
 
 module.exports.getALLSectors = catchAsync(async (req, res, next) => {
-  const sectors = await sectorModel.find({}).populate({
-    path: "children",
-    populate: {
+  const sectors = await sectorModel
+    .find({})
+    .populate({
       path: "children",
-      model: "SubSector",
       populate: {
         path: "children",
-        model: "SubSector",
+        populate: "children",
       },
-    },
-  }); // Populate the 'children' field in the nested documents
+    })
+    .lean(); // Use lean for better performance
+
   res.status(200).json(sectors);
 });
